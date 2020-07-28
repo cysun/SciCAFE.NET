5e6308548d020c06f29e227bd94e5a561ad7a0d9
Chengyu Sun <csun@calstatela.edu>
2020-07-28 16:46:36 -0700

    Add project files.

---
+++ SciCAFE.NET/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SciCAFE.NET.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(255)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(255)]
+        public string LastName { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Email { get; set; }
+
+        public bool IsAdmin { get; set; }
+        public bool IsEventOrganizer { get; set; }
+        public bool IsRewardProvider { get; set; }
+
+        public bool Enabled { get; set; } = true;
+    }
+}
---
+++ SciCAFE.NET/Services/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SciCAFE.NET.Models;
+
+namespace SciCAFE.NET.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+            modelBuilder.Entity<User>().Property(u => u.Enabled).HasDefaultValue(true);
+        }
+    }
+}
