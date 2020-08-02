38e134a8d6a999ba4ffcaddf41f4030394a9945c
Chengyu Sun <csun@calstatela.edu>
2020-08-02 11:16:27 -0700

    Visual tweaks

--- SciCAFE.NET/Controllers/HomeController.cs
+++ SciCAFE.NET/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace SciCAFE.NET.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
